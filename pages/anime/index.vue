39fe28a65189fae8afd58b3c14d80086e3309b44
aftab500apps <aftab.500apps@gmail.com>
2024-02-02 12:36:30 +0530

    pagination added

--- pages/anime/index.vue
+++ pages/anime/index.vue
@@ -6,11 +6,32 @@
 
     <section
       class="m-auto grid justify-center items-center grid-cols-4 gap-5 p-4"
-      v-if="animeStore.animeData?.data"
+      v-if="animeData?.data"
     >
-      <ShowCard v-for="item in animeStore.animeData.data" :item="item" />
+      <ShowCard v-for="item in animeData.data" :item="item" />
     </section>
   </section>
+
+  <CommonRuleComponent />
+
+  <div class="pagination flex justify-center mb-4 items-center">
+    <!-- <button class="border-2" @click="goToPage(animeStore.currentPage - 1)"> -->
+    <CommonButton
+      class="flex w-24 justify-center rounded-md bg-red-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500"
+      @click="goToPage(currentPage - 1)"
+      :disabled="currentPage === 1"
+    >
+      Previous
+    </CommonButton>
+    <span class="p-2 font-bold">{{ currentPage }}</span>
+    <CommonButton
+      class="flex w-24 justify-center rounded-md bg-red-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500"
+      @click="goToPage(currentPage + 1)"
+      :disabled="currentPage >= totalPages"
+    >
+      Next
+    </CommonButton>
+  </div>
 </template>
 
 <script setup>
@@ -18,11 +39,25 @@ definePageMeta({
   middleware: "auth",
   layout: "custom",
 });
+import { toRefs, onMounted } from "vue";
 import { useAnimeStore } from "~/stores/useAnimeStore";
+// const animeStore = useAnimeStore();
+// const currentPage = ref(animeStore.currentPage);
+const { animeData, currentPage, totalPages } = toRefs(useAnimeStore());
 
-const animeStore = useAnimeStore();
-
-onMounted(async () => {
-  await animeStore.fetchData();
+onMounted(() => {
+  useAnimeStore().fetchData();
 });
+
+const goToPage = async (page) => {
+  // console.log("totalPages", totalPages.value);
+  // console.log("current", currentPage.value);
+  // console.log("going to", page);
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page;
+    await useAnimeStore().fetchData();
+  }
+};
+// console.log(animeStore.currentPage);
+// console.log(animeStore.itemsPerPage);
 </script>
--- stores/useAnimeStore.ts
+++ stores/useAnimeStore.ts
@@ -4,14 +4,26 @@ export const useAnimeStore = defineStore("animeapi", () => {
   const animeData = ref(null);
   const animeDetails = ref(null);
   const animeTop = ref(null);
+  const currentPage = ref(1);
+  const itemsPerPage = 10;
+  const totalPages = ref(1);
 
   const fetchData = async () => {
     try {
-      const data = await fetch("https://api.jikan.moe/v4/anime");
+      const data = await fetch(
+        `https://api.jikan.moe/v4/anime?page=${
+          currentPage.value
+        }&limit=${itemsPerPage}`
+      );
       const res = await data.json();
+
+      // console.log("api response", res);
+      // console.log(res.pagination.last_visible_page);
+      totalPages.value = res.pagination.last_visible_page;
       animeData.value = res;
-      return res;
+      return { res, totalPages };
     } catch (error) {
+      console.log(error);
       throw error;
     }
   };
@@ -44,5 +56,8 @@ export const useAnimeStore = defineStore("animeapi", () => {
     fetchDataById,
     fetchTopAnime,
     animeTop,
+    currentPage,
+    totalPages,
+    itemsPerPage,
   };
 });
